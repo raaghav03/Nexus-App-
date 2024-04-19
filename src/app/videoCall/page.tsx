562b881acc30d605c16511d5c5eb506fee1e05e0
raaghav03 <raghavnagpal2003@gmail.com>
2024-04-19 13:36:10 +0530

    video call bug

--- src/app/videoCall/page.tsx
+++ src/app/videoCall/page.tsx
@@ -3,6 +3,6 @@ import dynamic from "next/dynamic";
 
 const DynamicVideoUI = dynamic(() => import("./videoui"), { ssr: false })
 export default function VideoCall() {
-    return (<><h1>Hello video calling ? </h1><DynamicVideoUI /></>)
+    return (<><DynamicVideoUI /></>)
 }
 
